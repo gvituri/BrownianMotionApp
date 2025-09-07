6c1653770dda6f240a142e7bb57ee1ebd267f26e
Gustavo Vituri <gustavovituri@gmail.com>
2025-09-07 09:38:34 -0300

    Add brownian motion service tests

--- BrownianMotionApp/Services/BrownianMotionService.cs
+++ BrownianMotionApp/Services/BrownianMotionService.cs
@@ -1,24 +1,22 @@
 ﻿using BrownianMotionApp.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BrownianMotionApp.Services {
-    public class BrownianMotionService : IBrownianMotionService {
-        public double[] GenerateBrownianMotion(double sigma, double mean, double initialPrice, int numDays) {
-            Random rand = new();
-            double[] prices = new double[numDays];
-            prices[0] = initialPrice;
-            for (int i = 1; i < numDays; i++) {
-                double u1 = 1.0 - rand.NextDouble();
-                double u2 = 1.0 - rand.NextDouble();
-                double z = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
-                double dailyReturn = mean + sigma * z;
-                prices[i] = prices[i - 1] * Math.Exp(dailyReturn);
-            }
-            return prices;
+public class BrownianMotionService : IBrownianMotionService {
+    private readonly Random _rand;
+
+    public BrownianMotionService(Random? rand = null) {
+        _rand = rand ?? new Random();
+    }
+
+    public double[] GenerateBrownianMotion(double sigma, double mean, double initialPrice, int numDays) {
+        double[] prices = new double[numDays];
+        prices[0] = initialPrice;
+        for (int i = 1; i < numDays; i++) {
+            double u1 = 1.0 - _rand.NextDouble();
+            double u2 = 1.0 - _rand.NextDouble();
+            double z = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
+            double dailyReturn = mean + sigma * z;
+            prices[i] = prices[i - 1] * Math.Exp(dailyReturn);
         }
+        return prices;
     }
 }
---
+++ BrownianMotionAppTests/Services/BrownianMotionServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrownianMotionAppTests.Services {
+    public class BrownianMotionServiceTests {
+        [Fact]
+        public void GenerateBrownianMotion_ReturnsCorrectLength() {
+            var service = new BrownianMotionService(new Random(42));
+            int numDays = 100;
+            var result = service.GenerateBrownianMotion(0.2, 0.05, 100.0, numDays);
+
+            Assert.Equal(numDays, result.Length);
+        }
+
+        [Fact]
+        public void GenerateBrownianMotion_FirstPriceEqualsInitial() {
+            var service = new BrownianMotionService(new Random(42));
+            var result = service.GenerateBrownianMotion(0.2, 0.05, 100.0, 50);
+
+            Assert.Equal(100.0, result[0]);
+        }
+
+        [Fact]
+        public void GenerateBrownianMotion_AllPricesPositive() {
+            var service = new BrownianMotionService(new Random(42));
+            var result = service.GenerateBrownianMotion(0.2, 0.05, 100.0, 50);
+
+            Assert.All(result, price => Assert.True(price > 0));
+        }
+
+        [Fact]
+        public void GenerateBrownianMotion_ReproducibleWithSeed() {
+            var service1 = new BrownianMotionService(new Random(42));
+            var service2 = new BrownianMotionService(new Random(42));
+
+            var result1 = service1.GenerateBrownianMotion(0.2, 0.05, 100.0, 10);
+            var result2 = service2.GenerateBrownianMotion(0.2, 0.05, 100.0, 10);
+
+            Assert.Equal(result1, result2);
+        }
+    }
+}
